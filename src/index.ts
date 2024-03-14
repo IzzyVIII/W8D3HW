7ca2db247c130865815407788957d4a4b70896cd
IzzyVIII <eagb08@gmail.com>
2024-03-13 19:06:35 -0500

    Final Commit

--- src/index.ts
+++ src/index.ts
@@ -1,166 +1,61 @@
-import { v4 as uuidv4 } from 'uuid';
 //import?
+import { Character, TaskManager } from './CharacterManager'; 
+import {   Weapon} from './types'; 
 
-//export?
-export class Weapon {
-    readonly id: string;
-    name: string;
-    description?: string;
-    damagePoints: number;
+const taskForm = document.getElementById('taskForm') as HTMLFormElement;
+// // Add the new task to the task manager
+const taskManager = new TaskManager()
 
-    constructor(name: string, damagePoints: number, description?: string) {
-        this.id = uuidv4();
-        this.name = name;
-        this.damagePoints = damagePoints;
-        this.description = description;
-    }
-}
+const weaponForm = document.getElementById('weaponForm') as HTMLFormElement;
 
+weaponForm.addEventListener('submit', (event) => {
+    console.log('weaponForm');
+    
+    event.preventDefault(); // Prevent the default form submission behavior
 
-interface Attack {
-    attack(): void
-}
+    const weaponNameInput = document.getElementById('weaponName') as HTMLInputElement;
+    const descriptionInput = document.getElementById('weaponDescription') as HTMLTextAreaElement;
+    const weaponDamageInput = document.getElementById('weaponDamage') as HTMLInputElement;
 
-interface Defences {
-    defend(): void
-}
+    // Get the input values
+    let weaponName = weaponNameInput.value;
+    let description = descriptionInput.value;
+    let weaponDamagePoint = weaponDamageInput.value;
 
-interface GoldCollect {
-    gold(): void
-}
+    console.log(weaponName, description, weaponDamagePoint); // Log the input values
 
-class Club implements Attack {
-    attack() {
-        console.log("Club")
-    }
-}
+    // Create a new task
+    const newWeapon = new Weapon(weaponName, parseInt(weaponDamagePoint), description);
 
-class Sword implements Attack {
-    attack() {
-        console.log("Sword")
-    }
-}
+    
+    taskManager.addWeapon(newWeapon);
 
-class Bow implements Attack {
-    attack() {
-        console.log("Bow & Arrow")
-    }
-}
+    weaponForm.reset(); 
+    
+});
 
-class Shield implements Defences {
-    defend() {
-        console.log("Shield")
-    }
-}
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-class Armor implements Defences {
-    defend() {
-        console.log("Armor")
-    }
-}
+    const taskNameInput = document.getElementById('taskName') as HTMLInputElement;
+    const descriptionInput = document.getElementById('description') as HTMLTextAreaElement;
+    const weaponSelect = document.getElementById('category') as HTMLSelectElement;
 
-class Tunic implements Defences {
-    defend() {
-        console.log("Tunic")
-    }
-}
-
-class Gold implements GoldCollect {
-    gold() {
-        console.log("Collected Gold")
-    }
-}
-
-abstract class Character implements Defences, Attack, GoldCollect {
-    protected defendAbility$: Defences
-    protected attackAbility$: Attack
-    protected goldAbility$: GoldCollect
-
-    constructor(protected character_name$: string, goldAbility$: GoldCollect, attackAbility$: Attack, defendAbility$: Defences) {
-        this.goldAbility$ = goldAbility$
-        this.attackAbility$ = attackAbility$
-        this.defendAbility$ = defendAbility$
-
-    }
-
-    get krtr() { return this.character_name$ }
-
-    set defendAbility(da: Defences) {
-        this.defendAbility$ = da
-    }
-
-    set attackAbility(aa: Attack) {
-        this.attackAbility = aa
-    }
-
-    set goldAbility(ga: GoldCollect) {
-        this.goldAbility$ = ga
-    }
-
-    attack(): void {
-        this.attackAbility$.attack()
-    }
-
-    defend(): void {
-        this.defendAbility$.defend()
-    }
-
-    gold(): void {
-        this.goldAbility$.gold()
-    }
-}
-
-class Orges extends Character {
-
-    constructor(character_name$: string, goldAbility$: GoldCollect, attackAbility$: Attack, defendAbility$: Defences) {
-        super(character_name$, goldAbility$, attackAbility$, defendAbility$)
-    }
-
-}
-
-class Peons extends Character {
-
-    constructor(character_name$: string, goldAbility$: GoldCollect, attackAbility$: Attack, defendAbility$: Defences) {
-        super(character_name$, goldAbility$, attackAbility$, defendAbility$)
-    }
-}
-
-class Knights extends Character {
-    constructor(character_name$: string, goldAbility$: GoldCollect, attackAbility$: Attack, defendAbility$: Defences) {
-        super(character_name$, goldAbility$, attackAbility$, defendAbility$)
-    }
-}
+    // Get the input values
+    let taskName = taskNameInput.value;
+    let description = descriptionInput.value;
+    let index = weaponSelect.selectedIndex;
+    let weaponId = weaponSelect.options[index].id;
 
 
-class Archers extends Character {
-    constructor(character_name$: string, goldAbility$: GoldCollect, attackAbility$: Attack, defendAbility$: Defences) {
-        super(character_name$, goldAbility$, attackAbility$, defendAbility$)
-    }
-    // constructor(){
-    //     super("" , new Gold(), new Bow(), new Tunic())
-    // }
-}
+    console.log(taskName, description, weaponId); // Log the input values
 
+    // Create a new task
+    const newTask = new Character(taskName, description, weaponId);
 
-let allCharacters: Character[] = [
-    new Orges("Orge", new Gold(), new Bow(), new Tunic()),
-    new Peons("Peon", new Gold(), new Club(), new Armor()),
-    new Knights("Knight", new Gold(), new Sword(), new Shield()),
-    new Archers("Archer", new Gold(), new Bow(), new Tunic())
-]
+    
+    taskManager.addTask(newTask);
 
-for (let character of allCharacters) {
-    console.log(character.krtr)
-    character.attack()
-    character.defend()
-    character.gold()
-}
-
-
-    // protected defendAbility$: Defences | undefined
-    // protected attackAbility$: Attack | undefined
-    // protected goldAbility$: GoldCollect | undefined
-
-    // let Knightsv = new Knights("aa",new Gold(), new Bow(), new Tunic()) ---- Option
-
-    // let arch = new Archers()
+    taskForm.reset(); 
+    
+});
